a73531ead665dd435913430bda80dd628b136eca
Eduardo Lino Silva <eduardolinobjl@gmail.com>
2025-01-18 22:59:52 -0300

    Definindo os pinos dos leds/buzzer e adicionando o controle dos botãos

--- Tarefa2.c
+++ Tarefa2.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-// Definições dos LEDs RGB
-#define LED_RED 13
-#define LED_BLUE 12
-#define LED_GREEN 11
+// Definições dos LEDs RGB e Buzzer
+#define LED_GREEN_PIN 11
+#define LED_BLUE_PIN 12
+#define LED_RED_PIN 13
+#define BUZZER_PIN 21
 
-// Definição do buzzer
-#define BUZZER 21
-
-
-int main()
-{
-    stdio_init_all();
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+void desligar_todos_leds() {
+    gpio_put(LED_GREEN_PIN, 0);
+    gpio_put(LED_BLUE_PIN, 0);
+    gpio_put(LED_RED_PIN, 0);
+}
+void ligar_led_verde() {
+    desligar_todos_leds();
+    gpio_put(LED_GREEN_PIN, 1)
+}
+void ligar_led_azul() {
+    desligar_todos_leds();
+    gpio_put(LED_BLUE_PIN, 1)
+}
+void ligar_led_azul() {
+    desligar_todos_leds();
+    gpio_put(LED_RED_PIN, 1)
+}
+void ligar_todos_leds() {
+    gpio_put(LED_GREEN_PIN, 1);
+    gpio_put(LED_BLUE_PIN, 1);
+    gpio_put(LED_RED_PIN, 1);
+}
+void acionar_buzzer() {
+    gpiou_put(BUZZER_PIN, 1);
+    sleep_ms(2000);
+    gpiou_put(BUZZER_PIN, 0);
 }
